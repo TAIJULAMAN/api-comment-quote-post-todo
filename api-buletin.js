e8388eb8fafac62ec23811c5bd920a922295136c
TAIJULAMAN <taijulaman8@gmail.com>
2023-07-01 19:54:36 +0600

    api practice

---
+++ api-buletin.js
@@ -0,0 +1,27 @@
+// const headerSection = document.getElementById('header');
+
+
+
+//  first-header
+const firstHeader = document.getElementById('first-header');
+firstHeader.innerText = `What is an API?`;
+// first-paragraph
+const firstParagraph = document.getElementById('first-paragraph');
+firstParagraph.innerText = `APIs are used in web applications to connect user-facing front ends with critical back-end functionality and data. APIs are used to deliver material by streaming services such as Spotify and Netflix. APIs are used by automakers such as Tesla to provide software upgrades.</p>`
+
+// second-header
+const secondHeader = document.getElementById('second-header');
+secondHeader.innerText = `How does API works?`;
+// second-paragraph
+const secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.innerText = ` APIs are any software component that serves as an intermediary between two disconnected applications. While web services also connect applications, they require a network to do so. Where some APIs are open source, web services are typically private and only approved partners may access them.`
+// third-header
+const thirdHeader = document.getElementById('third-header');
+thirdHeader.innerText = `What are the 4 types of API?`;
+// list item
+const listItem = document.getElementById('list-item');
+listItem.innerHTML = `<li>Monolithic APIs.</li>
+                        <li>Microservices APIs.</li>
+                        <li>Composite APIs.</li>
+                        <li>Unified APIs.</li>`;
+
